248e34a5bacca53cc1c5b90478a32df171af298b
hasnainraza1212 <hr961992@gmail.com>
2024-03-29 23:39:45 +0500

    ok

--- src/Views/Layout/Layout.jsx
+++ src/Views/Layout/Layout.jsx
@@ -37,7 +37,7 @@ const Layout = () => {
       }
         const res = await getLoggedInUser(localStorage.getItem("token"));
 
-      if (res.success) {
+      if (res && res.success) {
         setUser(res.user);
         setIsLoading(false);
         return setIsAuthenticated(true);
